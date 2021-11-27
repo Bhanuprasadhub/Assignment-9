927a853131c12ecd18d86a2076a24b2cf39f599c
G Bhanu Prasad <gubbalausha11@gmail.com>
2021-11-27 17:05:26 +0530

    This is Assign-9

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -2,7 +2,8 @@ function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
     if ((hasDrivingLiscence === false)) {
         return "You cannot drive"
-    } if ((isTired === true) || (isSober === false)) {
+    }
+    if ((isTired === true) || (isSober === false)) {
         return "You shouldn't drive"
     }
     return "You can drive"
